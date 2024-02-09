09e28546d73e0d2befc57d0e40f9383b0d30845e
maximoamore2001 <maxiamorerc88@gmail.com>
2024-02-09 10:09:13 -0300

    cambios

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -147,7 +147,7 @@
         <div class="container-fluid">
         <div class="row">
             <div class="col-lg-12">
-                <h1 style="color: white;" class="page-header">@yield('titulo')</h1>
+                <h1 style="color: #000;" class="page-header">@yield('titulo')</h1>
             </div>
         </div>
         <div class="row">
