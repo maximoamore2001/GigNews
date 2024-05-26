17814e79ffc9e19f578ee694934b70bef2d4b8f6
Máximo Amore <maxiamorerc88@gmail.com>
2024-05-26 02:40:24 -0400

    cambios

--- resources/views/web/blog.blade.php
+++ resources/views/web/blog.blade.php
@@ -2,33 +2,33 @@
 @section("banner")
 
 
-<div class="properties mt-4">
+<div class="mt-5">
   <div class="container">
     <div class="col-12">
       <h1>Noticias</h1>
     </div>
-    @foreach($aBlogs as $blog)
     <div class="row mt-3">
+    @foreach($aBlogs as $blog)
     <div class="col-8">
       <div class="row">
       <div class="col-12 card p-3">
-        <img src="/files/{{ $blog->imagen }}" class="mb-2" alt="">
         <h3> {{ $blog->titulo }} </h3>
         <p> {{ $blog->fecha }} </p>
+        <img src="/files/{{ $blog->imagen }}" class="mb-2" alt="">
       </div>
       </div>
     </div>
-  @endforeach
+    <div class="col-4">
+      <div class="row">
+      <div class="col-12" >
+        <small> {{ $blog->fecha }} </small>
+        <p>{{ $blog->titulo }} </p>
+      </div>
+      </div>
+    </div>
+    @endforeach
+    </div>
 
-      <div class="col-4">
-        <div class="row">
-          <div class="col-12 pt-3" style="font-size: 18px;">
-            <small>{{ $blog->fecha }} |</small>
-            <a href=""> {{ $blog->titulo }} </a>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
 </div>
 
